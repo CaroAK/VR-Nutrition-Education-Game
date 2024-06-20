756011258983be71fdc53639591b2c8f46127c03
CaroAK <CarolineAliciaKlein@icloud.com>
2024-06-19 22:18:15 -0400

    Canvas UI

---
+++ VR Buffet/Assets/LabelScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabelScript : MonoBehaviour
+{
+
+    public void Select()
+    {
+        Debug.Log("Added to plate");
+    }
+}
