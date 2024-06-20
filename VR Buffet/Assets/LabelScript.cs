5aa2e08e4e0d24e30e1dbc4f8a5b0ab7b7b4b84f
CaroAK <CarolineAliciaKlein@icloud.com>
2024-06-20 18:49:56 -0400

    Screen overlay

--- VR Buffet/Assets/LabelScript.cs
+++ VR Buffet/Assets/LabelScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LabelScript : MonoBehaviour
 {
+    public PointBarScript pointbar;
 
     public void Select()
     {
         Debug.Log("Added to plate");
+        pointbar.SetPoints(5);
     }
 }
